d42dbe3d3d74a51dc28b1a80ad851eb888f58da2
Yc1997 <you@example.com>
2020-12-08 16:25:13 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//按需导入vant
+import '@/plugins/vant'
+//引入rem布局
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 new Vue({
